42f5128b8eaea0ee31831e041b96cac6551096c8
bobvanluijt <bob@kubrickolo.gy>
2018-06-08 15:25:06 +0200

    gh-81: Merge mailchimp and semi js

--- js/semi.js
+++ js/semi.js
@@ -50,6 +50,64 @@ if (loc.indexOf('http://')==0 && location.hostname !== "127.0.0.1" && location.h
     window.location.href = loc.replace('http://','https://');
 }
 
+// send request to mailchimp
+function toMailchimp(t){
+
+    // hide buttons
+    var buttons = document.getElementsByTagName('input');
+    for (var i = 0; i < buttons.length; i++) {
+        if(buttons[i].getAttribute("type")== "button"){
+            buttons[i].style.display = "none";
+        }
+    }
+
+    // send the data through
+    function sendData(data, callback) {
+
+        $.ajax({
+            type: "POST",
+            url:        "https://us-central1-semi-186012.cloudfunctions.net/mailchimp",
+            data:       data,
+            success:    callback(),
+            dataType:   "json",
+            cache:      false
+          });
+      
+    }
+
+    // get the value of ...
+    function getVal(i){
+        var elVal = document.getElementsByName(i)
+        if(elVal.length === 0){
+            return null
+        } else {
+            return elVal[0].value
+        }
+    }
+
+    // return to url
+    function returnTo(){
+        var protocol = location.protocol;
+        var slashes = protocol.concat("//");
+        return slashes.concat(window.location.hostname + ":" + window.location.port);
+    }
+
+    // get all fields
+    var formFields = {
+        "EMAIL"     : getVal("EMAIL"),
+        "NAME"      : getVal("NAME"),
+        "PRODUCT"   : getVal("PRODUCT"),
+        "MESSAGE"   : getVal("MESSAGE"),
+        "TYPE"      : t,
+        "RETURN"    : returnTo
+    }
+
+    sendData(formFields, function(){
+        window.location.href = "/thank-you.html";
+    })
+
+}
+
 // include Google Tagmanager
 (function(w, d, s, l, i) {
     w[l] = w[l] || [];
